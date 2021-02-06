d0122ecd3289e7e22a3a029f0075d3ebcb6e195b
MNems <tomlucas@live.fr>
2021-02-06 15:44:25 +0100

    Update main

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ import os
 import discord
 
 
-class BrassageBot:
+class BrassageBot(discord.Client):
 
-    def __init__(self, client, token) -> None:
-        super().__init__()
-        self.client = client
-        self.token = token
+    async def on_ready(self):
+        print("Début du développement de ...", self.user)
 
-        @client.event
-        async def say_hi():
-            print("Début du développement de ...", client.user)
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
 
-    def run(self):
-        self.client.run(self.token)
+        if "!brassage" in message.content:
+            await message.reply('Belle action ', mention_author=True)
 
 
 if __name__ == '__main__':
-    brassage_bot = BrassageBot(discord.Client(), os.getenv("DISCORD_TOKEN"))
-    brassage_bot.run()
+    brassage_bot = BrassageBot()
+    brassage_bot.run("ODA3NjIwODQ5MTI5NDg4Mzk1.YB6phw.GvUVf_ETtCgBU2UcYg-xF3AKRFs")
